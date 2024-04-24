a842edec2096c10b544487052e5fe4711adb6698
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-04-24 19:01:03 +0200

    [UPD] Added code to retrive the content of the element whith ID "prezzo"

---
+++ mains.js
@@ -0,0 +1,8 @@
+// reset js
+'use strict';
+
+// Salvo l'elemento paragrafo all'interno di una variabile
+let currentElement = document.getElementById('prezzo');
+
+// riciclo il valore iniziale della variabile
+let currentText = currentElement.innerHTML;

c69f73ac7ada49710d884b17ab4c8bdaa1743287
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-04-24 19:16:50 +0200

    [UPD] Conditional instructions, apply a discount of 20% if the user is under 18 years old, or 40% if he is over 65 years old.

--- mains.js
+++ mains.js
@@ -17,3 +17,21 @@ const UserAge = Number(prompt("Inserire la propria età: "));
 
 const StartingPrice = ChilometriDaPercorrere * 0.21;
 let sconto
+
+// Istruzioni condizionali : applica uno sconto del 20% se l'utente ha meno di 18 anni, o del 40% se ha più di 65 anni.
+
+if(UserAge < 18) {
+    
+    sconto = StartingPrice * (20 / 100);
+    console.log ("Applicato lo sconto del 20%, il prezzo del biglietto è:");
+
+
+}else if(UserAge > 65) {
+    
+    sconto = StartingPrice * (40 / 100);
+    console.log ("Applicato lo sconto del 40%, il prezzo del biglietto è:");
+
+}else {
+    sconto = 0;
+    console.log ("Non è stato applicato alcuno sconto, il prezzo del biglietto è:");
+}
